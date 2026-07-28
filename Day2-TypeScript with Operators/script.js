9f1052cd8d387349fef7bc3b034ccd0824e07da8
shanu-react-dev <amarjeet.kr.dev@gmail.com>
2026-07-28 13:40:00 +0530

    operators

--- Day2-TypeScript with Operators/script.js
+++ Day2-TypeScript with Operators/script.js
@@ -1,5 +1,4 @@
 console.log("hii today we are going to learn Operators with typescript");
-
 //! Operators
 //? Operators are predefined symbol used to perform some specific tasks based on operands.
 //! Arithmetic Operator
@@ -7,15 +6,12 @@ console.log("hii today we are going to learn Operators with typescript");
 // let a: number = 10;
 // let b: string = "10";
 // let c: undefined = undefined;
-
 // console.log(a + b);
 // console.log(a + c);
-
 //todo In arithmetic operator in TypeScript or JS if we are using addition with any datatype along with string it will perform concatenation apart from this any datatype apart from string will throw error in TypeScript
-
 //! Addition
-let a: number = 10;
-let b: string = "Shanu";
+let a = 10;
+let b = "Shanu";
 //? addition operator with string:- Any datatype with string will get concatenated except Symbol and bigint
 /**
  * console.log(a + b);
@@ -27,20 +23,14 @@ let b: string = "Shanu";
  * let arrOfFruits: string[] = ["guava", "pomegranate", "grapes", "orange"];
  * console.log(b + arrOfFruits);
  */
-
 //? Addition operator with other data types
 // let c = null;
 // console.log(a + c);
 // let d = undefined;
 // console.log(a + d);
-
 //! Subtraction
-let res: number | any = b - a;
+let res = b - a;
 console.log(res);
-
 res = isNaN(90);
 console.log(res);
 export {};
-
-//! use strict in JavaScript
-//? The "use strict" directive is a literal expression that enables "strict mode" in JavaScript, forcing the code to execute under a safer, more restricted set of rules. Introduced in ECMAScript 5 (ES5), it helps developers write cleaner code by transforming silent runtime errors into explicit thrown exceptions and preventing accidental actions like creating global variables.
